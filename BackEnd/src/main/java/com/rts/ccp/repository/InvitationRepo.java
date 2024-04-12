44c1feec026503bfcc9b3e66cf027ba3a65ad305
Jeyandhan <86904129+Jeyandh@users.noreply.github.com>
2024-04-12 15:12:52 +0530

    Delete BackEnd directory

--- BackEnd/src/main/java/com/rts/ccp/repository/InvitationRepo.java
+++
@@ -1,9 +0,0 @@
-package com.rts.ccp.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.rts.ccp.bean.Invitation;
-
-@Repository
-public interface InvitationRepo extends JpaRepository<Invitation, Long> {
-}
--- BackEnd/src/main/java/com/rts/ccp/repository/SymbolRepo.java
+++
@@ -1,9 +0,0 @@
-package com.rts.ccp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rts.ccp.bean.Symbol;
-
-public interface SymbolRepo extends CrudRepository<Symbol, Integer> {
-
-}
